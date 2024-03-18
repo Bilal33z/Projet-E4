baacb63972448e3d69409d570afe2b6964b60cd3
Bilal33z <159137515+Bilal33z@users.noreply.github.com>
2024-03-18 17:29:09 +0100

    Update serveur.c

--- serveur.c
+++ serveur.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -10,21 +10,21 @@
 #define PORT 8080
 
 int main() {
-    int server_fd, client_socket;
+    int server_fd, new_socket;
     struct sockaddr_in address;
     int opt = 1;
     int addrlen = sizeof(address);
     char buffer[1024] = {0};
-    
+
     // Creating socket file descriptor
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("socket failed");
+        perror("Socket failed");
         exit(EXIT_FAILURE);
     }
     
-    // Forcefully attaching socket to the port 8080
+    // Forcefully attaching socket to the port
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
-        perror("setsockopt");
+        perror("Setsockopt failed");
         exit(EXIT_FAILURE);
     }
     address.sin_family = AF_INET;
@@ -32,36 +32,34 @@ int main() {
     address.sin_port = htons(PORT);
     
     // Bind the socket to the address and port number.
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) {
-        perror("bind failed");
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("Bind failed");
         exit(EXIT_FAILURE);
     }
     
     // Listen for connections
     if (listen(server_fd, 3) < 0) {
-        perror("listen");
+        perror("Listen");
         exit(EXIT_FAILURE);
     }
     
     // Accept a connection
-    if ((client_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) {
-        perror("accept");
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+        perror("Accept");
         exit(EXIT_FAILURE);
     }
     
-    // Read and echo back the message from the client
-    read(client_socket, buffer, 1024);
+    // Read message from client
+    read(new_socket, buffer, 1024);
     printf("Message from client: %s\n", buffer);
     
-    // Here we would normally need to parse the WebSocket handshake, 
-    // encode our response as a WebSocket frame, etc.
-    // For demonstration, just send back a plain message (not WebSocket framed)
-    char *reply = "Echo from server: ";
-    send(client_socket, reply, strlen(reply), 0);
-    send(client_socket, buffer, strlen(buffer), 0);  // Echo back the received message
+    // Send a message back to client
+    char *message = "Hello from the C server!";
+    send(new_socket, message, strlen(message), 0);
+    printf("Message sent to client\n");
 
     // Close the socket
-    close(client_socket);
+    close(new_socket);
     close(server_fd);
 
     return 0;
