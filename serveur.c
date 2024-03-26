ce56ea808b7a9f16a9a773ead19f4701788b0d38
Bilal33z <159137515+Bilal33z@users.noreply.github.com>
2024-03-26 12:19:41 +0100

    Update serveur.c

--- serveur.c
+++ serveur.c
@@ -2,41 +2,41 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 
 #define PORT 8080
 #define BUFSIZE 8096
-#define ERROR_404 "HTTP/1.1 404 Not Found\nContent-Length: 136\nConnection: close\nContent-Type: text/html\n\n<html><head>\n<title>404 Not Found</title>\n</head><body>\n<h1>Not Found</h1>\nThe requested URL was not found on this server.\n</body></html>\n"
+#define WEBROOT "/home/kali/Downloads/Projet-E4-main"
 
-void handle_http_request(int client_fd) {
-    char buf[BUFSIZE], *method, *uri, *version;
-    read(client_fd, buf, BUFSIZE);
-
-    method = strtok(buf, " ");
-    uri = strtok(NULL, " ");
-    version = strtok(NULL, "\r");
-
-    printf("%s %s %s\n", method, uri, version);
-
-    if (strcmp(uri, "/") == 0) uri = "/index.html"; // Si l'URI est juste "/", servez index.html
-
-    // Construisez le chemin du fichier en préfixant le dossier où se trouvent vos fichiers web
+void serveFile(int client_fd, const char *uri) {
     char filepath[1024];
-    sprintf(filepath, "web%s", uri);
+    char buffer[BUFSIZE];
+    FILE *file;
 
-    FILE *file = fopen(filepath, "r");
+    // Construction du chemin absolu vers le fichier
+    snprintf(filepath, sizeof(filepath), "%s%s", WEBROOT, uri);
+
+    // Si l'URI est juste "/", servir index.html
+    if (strcmp(uri, "/") == 0) {
+        snprintf(filepath, sizeof(filepath), "%s/index.html", WEBROOT);
+    }
+
+    file = fopen(filepath, "rb");
     if (file == NULL) {
-        write(client_fd, ERROR_404, strlen(ERROR_404));
+        // Envoyer une réponse 404 Not Found si le fichier n'existe pas
+        char *header = "HTTP/1.1 404 Not Found\n\n404 Not Found\n";
+        write(client_fd, header, strlen(header));
     } else {
-        // Envoyer l'entête de réponse HTTP 200 OK
-        char header[1024];
-        sprintf(header, "HTTP/1.1 200 OK\n\n");
+        // Envoyer l'en-tête HTTP OK avant le contenu du fichier
+        char *header = "HTTP/1.1 200 OK\n\n";
         write(client_fd, header, strlen(header));
 
         // Lire le fichier et l'envoyer en blocs
         int bytes_read;
-        while ((bytes_read = fread(buf, 1, BUFSIZE, file)) > 0) {
-            write(client_fd, buf, bytes_read);
+        while ((bytes_read = fread(buffer, 1, sizeof(buffer), file)) > 0) {
+            write(client_fd, buffer, bytes_read);
         }
         fclose(file);
     }
@@ -46,42 +46,52 @@ int main() {
     int server_fd, client_fd;
     struct sockaddr_in server_addr, client_addr;
     socklen_t sin_len = sizeof(client_addr);
+    char buffer[BUFSIZE];
+    char *method, *uri;
 
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd < 0) {
-        perror("socket");
-        exit(1);
+        perror("Cannot open socket");
+        exit(EXIT_FAILURE);
     }
 
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(PORT);
 
-    if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) == -1) {
-        perror("bind");
+    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+        perror("Cannot bind");
         close(server_fd);
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 
-    if (listen(server_fd, 5) < 0) {
-        perror("listen");
+    if (listen(server_fd, 10) == -1) {
+        perror("Cannot listen");
         close(server_fd);
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 
-    printf("Server is listening on port %d\n", PORT);
+    printf("Server is listening on %d\n", PORT);
 
     while (1) {
-        client_fd = accept(server_fd, (struct sockaddr *) &client_addr, &sin_len);
+        client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &sin_len);
         if (client_fd == -1) {
-            perror("Connection failed...\n");
+            perror("Cannot accept");
             continue;
         }
 
-        handle_http_request(client_fd);
+        // Lire la requête HTTP
+        read(client_fd, buffer, BUFSIZE);
+        method = strtok(buffer, " ");
+        uri = strtok(NULL, " ");
+        printf("Requested URI: %s\n", uri);
+
+        // Servir le fichier demandé
+        serveFile(client_fd, uri);
+
+        // Fermer la connexion client
         close(client_fd);
     }
 
-    close(server_fd);
     return 0;
 }
