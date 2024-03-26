735c42655cac5f507988c87ac290b2f13261beda
Bilal33z <159137515+Bilal33z@users.noreply.github.com>
2024-03-26 11:38:34 +0100

    Update serveur.c

--- serveur.c
+++ serveur.c
@@ -1,66 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <netdb.h>
+#include <ifaddrs.h>
 #include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
-
-#define PORT 8080
-
-int main() {
-    int server_fd, new_socket;
-    struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
-    char buffer[1024] = {0};
-
-    // Creating socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("Socket failed");
+#include <sys/socket.h>
+int main()
+{
+    struct ifaddrs *addr, *intf;
+    char hostname[NI_MAXHOST];
+    int family, s;
+    
+    if (getifaddrs(&intf) == -1) {
+        perror("getifaddrs");
         exit(EXIT_FAILURE);
     }
-    
-    // Forcefully attaching socket to the port
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
-        perror("Setsockopt failed");
-        exit(EXIT_FAILURE);
+    for (addr = intf; addr != NULL; addr = addr->ifa_next) {
+        family = addr->ifa_addr->sa_family;
+    //AF_INET est la famille d'adresses pour IPv4
+        if (family == AF_INET) {
+          //getnameinfo permet la résolution de noms.
+          s = getnameinfo(addr->ifa_addr, 
+                          sizeof(struct sockaddr_in),
+                          hostname, 
+                          NI_MAXHOST, 
+                          NULL, 
+                          0, 
+                          NI_NUMERICHOST);
+          printf("<interface>: %s \t <adresse> %s\n", addr->ifa_name, hostname);
+        }
     }
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
-    
-    // Bind the socket to the address and port number.
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("Bind failed");
-        exit(EXIT_FAILURE);
-    }
-    
-    // Listen for connections
-    if (listen(server_fd, 3) < 0) {
-        perror("Listen");
-        exit(EXIT_FAILURE);
-    }
-    
-    // Accept a connection
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
-        perror("Accept");
-        exit(EXIT_FAILURE);
-    }
-    
-    // Read message from client
-    read(new_socket, buffer, 1024);
-    printf("Message from client: %s\n", buffer);
-    
-    // Send a message back to client
-    char *message = "Hello from the C server!";
-    send(new_socket, message, strlen(message), 0);
-    printf("Message sent to client\n");
-
-    // Close the socket
-    close(new_socket);
-    close(server_fd);
-
     return 0;
 }
